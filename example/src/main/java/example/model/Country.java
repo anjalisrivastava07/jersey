aaee1da0a8d0c6f43a2929048160faef8ba66bd2
anjalisrivastava07 <113108631+anjalisrivastava07@users.noreply.github.com>
2022-11-23 15:43:46 +0530

    Add files via upload

---
+++ example/src/main/java/example/model/Country.java
@@ -0,0 +1,28 @@
+package example.model;
+
+public class Country {
+	int id;
+	String countryName;
+	public Country(int id, String countryName) {
+		super();
+		this.id = id;
+		this.countryName = countryName;
+	}
+	public Country() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	
+
+}
